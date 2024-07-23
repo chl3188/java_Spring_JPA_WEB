ec4c9f5deffad4e7e2097821a93efea9248f85f2
luji <chl3188@gmail.com>
2024-07-23 13:51:10 +0900

    스프링 데이터 JPA Named Parameter와 SpEL

--- src/main/java/me/luji/springjpaweb/post/PostRepository.java
+++ src/main/java/me/luji/springjpaweb/post/PostRepository.java
@@ -3,6 +3,7 @@ package me.luji.springjpaweb.post;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +11,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByTitleStartingWith(String title);
 
-    @Query("SELECT p From Post p where p.title = ?1")
-    List<Post> findByTitle(String title, Sort sort);
+    @Query("SELECT p From #{#entityName} p where p.title = :title")
+    List<Post> findByTitle(@Param("title") String title, Sort sort);
 }
